fe992c093415ab3bdd1a526b5818b4e41b7532c9
Jaeyoon Han <otzslayer@gmail.com>
2025-01-14 14:25:38 +0900

    feat: division and multiplication features done

--- tests/test_money.py
+++ tests/test_money.py
@@ -1,12 +1,6 @@
 import pytest
 
-
-class Dollar:
-    def __init__(self, amount):
-        self.amount = amount
-
-    def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
+from money import Dollar, Money
 
 
 def test_multiplication():
@@ -14,3 +8,34 @@ def test_multiplication():
     tenner = fiver.times(2)
 
     assert tenner.amount == 10
+
+
+def test_multiplication_in_dollars():
+    five_dollars = Money(5, "USD")
+    ten_dollars = Money(10, "USD")
+
+    assert ten_dollars == five_dollars.times(2)
+
+
+def test_multiplication_in_euros():
+    ten_euros = Money(10, "EUR")
+    twenty_euros = ten_euros.times(2)
+
+    assert 20 == twenty_euros.amount
+    assert "EUR" == twenty_euros.currency
+
+
+def test_division():
+    original_money = Money(4002, "KRW")
+    actual_money_after_division = original_money.divide(4)
+    expected_money_after_division = Money(1000.5, "KRW")
+
+    assert (
+        expected_money_after_division.amount
+        == actual_money_after_division.amount
+    )
+
+    assert (
+        expected_money_after_division.currency
+        == actual_money_after_division.currency
+    )
